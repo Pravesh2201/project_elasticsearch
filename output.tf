0785364ce289ebd398072236fc8431352278eff8
Pravesh Kumar <161683443+Pravesh2201@users.noreply.github.com>
2024-09-24 01:40:45 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -10,3 +10,10 @@ output "private_instance_id" {
   value = module.ec2.private_instance_id
 }
 
+output "public_instance_ip" {
+  value = aws_instance.public_instance.public_ip
+}
+
+output "private_instance_ip" {
+  value = aws_instance.private_instance.private_ip
+}
