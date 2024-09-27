a388733796006fdd6d1f6e1d671cfd485945344d
Pravesh Kumar <161683443+Pravesh2201@users.noreply.github.com>
2024-09-27 23:52:13 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "elasticsearch-tool"      # Your S3 bucket name
+    bucket         = "elasticsearch-tool1"      # Your S3 bucket name
     key            = "terraform/statefile.tfstate"  # Path to store state file in S3
     region         = "us-east-1"               # AWS region
     encrypt        = true                      # Enable encryption
