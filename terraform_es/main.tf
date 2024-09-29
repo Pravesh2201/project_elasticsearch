e227ba7854438f0767ba09b766c5b064771d366a
Pravesh Kumar <161683443+Pravesh2201@users.noreply.github.com>
2024-09-29 19:44:06 +0530

    Update main.tf

--- terraform_es/main.tf
+++ terraform_es/main.tf
@@ -54,23 +54,4 @@ module "ec2" {
 
 }
 
-# DynamoDB Table for State Locking (Recommended for S3 backend)
-#resource "aws_dynamodb_table" "terraform_lock" {
-#  name           = "terraform-lock-table"
-#  billing_mode   = "PAY_PER_REQUEST"
-#  hash_key       = "LockID"
-
-#  attribute {
-#    name = "LockID"
-#    type = "S"
-#  }
-
-#  tags = {
-#    Name = "Terraform Lock Table"
-#  }
-#  lifecycle {
-#    prevent_destroy = true
-#    ignore_changes =  all 
-#  }
-#}
 
